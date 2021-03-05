7622f64b277642fa6a7a1e2e94d92a35733c329f
Giulio Monaco <62191096+Grake17@users.noreply.github.com>
2021-03-05 17:23:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("ready", () =>{
     client.user.setActivity(args, {type: "WATCHING"});
 })
 
-client.login("Njg2ODg3NDcyMTIyNDI5NDk2.Xmdvzw.Gttt5Ln4SlrpcKcjJ5Ps0XVmj88")
+client.login("tokenhere")
 
 //Var Compagnie
 const compagnie = require("./clan/compagnie")
